a119434c57d258115407151f3eda0e79b714ba20
Daniel Elias Fonseca Rumin <43758059+MrBElga@users.noreply.github.com>
2023-03-14 11:28:39 -0300

    formatado

--- alpha/main.c
+++ alpha/main.c
@@ -4,98 +4,95 @@
 #include <windows.h>
 #include "tad.h"
 
-
-int main(){
-    //define tamanho do prompt e o titulo do prompt
+int main()
+{
+    // define tamanho do prompt e o titulo do prompt
     system("MODE con cols=95 lines=35");
     system("title Simulacao de programa em C");
-    textColor(BLACK,_DARKGRAY);
-  
+    textColor(BLACK, _DARKGRAY);
+
     telainicial();
-    
-    char op, arqName[30],flag='0';
+
+    char op, arqName[30], flag = '0';
     Coluna *Col;
     Linha *Lin;
 
     initListaC(&Col);
     initListaL(&Lin);
 
-    do{
+    do
+    {
         fflush(stdin);
-        op=getche();
-     
-            if(op==65)
+        op = getche();
+
+        if (op == 65)
+        {
+            system("cls");
+            box(2, 2, 12, 94);
+            linhaCol(6, 4);
+            printf("Nome Do Arquivo:");
+            // lin1, col1, lin2, col2
+            box(5, 21, 7, 93);
+            linhaCol(6, 22);
+            fflush(stdin);
+            gets(arqName);
+
+            while (strcmp(arqName, "") == 0)
             {
-                system("cls");
-                box(2,2,12,94);
-                linhaCol(6,4);
-                printf("Nome Do Arquivo:");
-                //lin1, col1, lin2, col2
-                box(5,21,7,93);
-                linhaCol(6,22);
+                linhaCol(6, 22);
                 fflush(stdin);
                 gets(arqName);
+            }
 
-                while(strcmp(arqName,"")==0)
+            // deve ser implementadp em uma lista de listas
+            while (flag != '1')
+            {
+                FILE *arq = fopen(arqName, "r");
+                if (arq)
                 {
-                    linhaCol(6,22); 
+                    flag = '1';
+                }
+                else
+                {
+                    linhaCol(8, 25);
+                    textColor(RED, _DARKGRAY);
+                    printf("ERRO: verifique se o arquivo existe ou se eh um .C");
+                    linhaCol(6, 22);
+                    printf("                                                                       ");
+                    linhaCol(6, 22);
+                    textColor(BLACK, _DARKGRAY);
+
                     fflush(stdin);
                     gets(arqName);
-                }
 
-                //deve ser implementadp em uma lista de listas                   
-                while(flag != '1')
-                {
-                    FILE *arq = fopen(arqName,"r");
-                    if(arq)
+                    while (strcmp(arqName, "") == 0)
                     {
-                        flag = '1';
-                  
-                    }
-                    else
-                    {
-                        linhaCol(8,25);
-                        textColor(RED,_DARKGRAY);
-                        printf("ERRO: verifique se o arquivo existe ou se eh um .C");
-                        linhaCol(6,22);
-                        printf("                                                                       ");
-                        linhaCol(6,22)  ;
-                        textColor(BLACK,_DARKGRAY);
-
-                        fflush(stdin);
+                        linhaCol(6, 22);
                         gets(arqName);
-                        
-                        while(strcmp(arqName,"")==0)
-                        {
-                            linhaCol(6,22);  
-                            gets(arqName);
-                        }
                     }
-                    fclose(arq);
                 }
-                gerarLista(&Col, &Lin, arqName);
-                telainicial();
-                flag = '0';
-               
-            }    
-            else if(op==66){
-                system("cls");
-                telainicial();
+                fclose(arq);
             }
-            else if(op==67){  
-                system("cls");  
-                telaRam();
-               
+            gerarLista(&Col, &Lin, arqName);
+            telainicial();
+            flag = '0';
+        }
+        else if (op == 66)
+        {
+            system("cls");
+            telainicial();
+        }
+        else if (op == 67)
+        {
+            system("cls");
+            telaRam();
+        }
+        else if (op == 68)
+        {
+        }
 
-                
-            }  
-            else if(op==68){
+    } while (op != 27);
 
-                
-            }            
-        
-    }while(op!=27);
- 
     system("cls");
     printf("programa finalizado: ");
 
--- alpha/tad.h
+++ alpha/tad.h
@@ -4,188 +4,212 @@ void linhaCol(int lin, int col);
 void box(int lin1, int col1, int lin2, int col2);
 void textColor(int letras, int fundo);
 
-  //COR DA LETRA
-  enum{BLACK,                 //0
-       BLUE,                  //1
-       GREEN,                 //2
-       CYAN,                  //3
-       RED,                   //4
-       MAGENTA,               //5
-       BROWN,                 //6
-       LIGHTGRAY,             //7
-       DARKGRAY,              //8
-       LIGHTBLUE,             //9
-       LIGHTGREEN,            //10
-       LIGHTCYAN,             //11
-       LIGHTRED,              //12
-       LIGHTMAGENTA,          //13
-       YELLOW,                //14
-       WHITE                  //15
-       
-       };  
+// COR DA LETRA
+enum
+{
+    BLACK,        // 0
+    BLUE,         // 1
+    GREEN,        // 2
+    CYAN,         // 3
+    RED,          // 4
+    MAGENTA,      // 5
+    BROWN,        // 6
+    LIGHTGRAY,    // 7
+    DARKGRAY,     // 8
+    LIGHTBLUE,    // 9
+    LIGHTGREEN,   // 10
+    LIGHTCYAN,    // 11
+    LIGHTRED,     // 12
+    LIGHTMAGENTA, // 13
+    YELLOW,       // 14
+    WHITE         // 15
 
-  //COR DO FUNDO
-  enum{_BLACK=0,                 //0
-       _BLUE=16,                  //1
-       _GREEN=32,                 //2
-       _CYAN=48,                  //3
-       _RED=64,                   //4
-       _MAGENTA=80,               //5
-       _BROWN=96,                 //6
-       _LIGHTGRAY=112,             //7
-       _DARKGRAY=128,              //8
-       _LIGHTBLUE=144,             //9
-       _LIGHTGREEN=160,            //10
-       _LIGHTCYAN=176,             //11
-       _LIGHTRED=192,              //12
-       _LIGHTMAGENTA=208,          //13
-       _YELLOW=224,                //14
-       _WHITE=240                  //15       
-       };  
-       
-void textColor(int letra, int fundo){
-     SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), letra + fundo);
-}
-     
-void linhaCol(int lin, int col){
-     SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE),(COORD){col-1,lin-1});// coorddenada na tela
-     
-     //funcao para deixar o cursor invisivel
-     HANDLE consoleHandle = GetStdHandle(STD_OUTPUT_HANDLE);
-     CONSOLE_CURSOR_INFO info;
-     info.dwSize = 100;
-     info.bVisible = FALSE;
-     SetConsoleCursorInfo(consoleHandle, &info);
+};
+
+// COR DO FUNDO
+enum
+{
+    _BLACK = 0,          // 0
+    _BLUE = 16,          // 1
+    _GREEN = 32,         // 2
+    _CYAN = 48,          // 3
+    _RED = 64,           // 4
+    _MAGENTA = 80,       // 5
+    _BROWN = 96,         // 6
+    _LIGHTGRAY = 112,    // 7
+    _DARKGRAY = 128,     // 8
+    _LIGHTBLUE = 144,    // 9
+    _LIGHTGREEN = 160,   // 10
+    _LIGHTCYAN = 176,    // 11
+    _LIGHTRED = 192,     // 12
+    _LIGHTMAGENTA = 208, // 13
+    _YELLOW = 224,       // 14
+    _WHITE = 240         // 15
+};
+
+void textColor(int letra, int fundo)
+{
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), letra + fundo);
 }
 
-void box(int lin1, int col1, int lin2, int col2){
-    int i=0, j=0;
+void linhaCol(int lin, int col)
+{
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), (COORD){col - 1, lin - 1}); // coorddenada na tela
 
-    //setar tamanho da tela e titulo do CMD
-    
+    // funcao para deixar o cursor invisivel
+    HANDLE consoleHandle = GetStdHandle(STD_OUTPUT_HANDLE);
+    CONSOLE_CURSOR_INFO info;
+    info.dwSize = 100;
+    info.bVisible = FALSE;
+    SetConsoleCursorInfo(consoleHandle, &info);
+}
 
-    for(i=col1;i<=col2;i++){ //linhas 
-        linhaCol(lin1,i);
-        printf("%c",196);
-        linhaCol(lin2,i);
-        printf("%c",196);
-  
+void box(int lin1, int col1, int lin2, int col2)
+{
+    int i = 0, j = 0;
+
+    // setar tamanho da tela e titulo do CMD
+
+    for (i = col1; i <= col2; i++)
+    { // linhas
+        linhaCol(lin1, i);
+        printf("%c", 196);
+        linhaCol(lin2, i);
+        printf("%c", 196);
     }
 
-    for(i=lin1;i<=lin2;i++){ //colunas
-        
-        linhaCol(i,col1);
-        printf("%c",179);
-        linhaCol(i,col2);
-        printf("%c",179);
+    for (i = lin1; i <= lin2; i++)
+    { // colunas
+
+        linhaCol(i, col1);
+        printf("%c", 179);
+        linhaCol(i, col2);
+        printf("%c", 179);
     }
 
-    for(i=lin1+1;i<lin2;i++){//percorre caixa
-        for(j=col1+1;j<col2;j++){
-            linhaCol(i,j);
+    for (i = lin1 + 1; i < lin2; i++)
+    { // percorre caixa
+        for (j = col1 + 1; j < col2; j++)
+        {
+            linhaCol(i, j);
             printf(" ");
         }
     }
-    
-    //cantos
-    linhaCol(lin1,col1);
-    printf("%c",218);
-    linhaCol(lin1,col2);
-    printf("%c",191);
-    linhaCol(lin2,col1);
-    printf("%c",192);
-    linhaCol(lin2,col2);
-    printf("%c",217);
+
+    // cantos
+    linhaCol(lin1, col1);
+    printf("%c", 218);
+    linhaCol(lin1, col2);
+    printf("%c", 191);
+    linhaCol(lin2, col1);
+    printf("%c", 192);
+    linhaCol(lin2, col2);
+    printf("%c", 217);
 }
 
-void telainicial(){
-  //lin1, col1, lin2, col2
-    box(2,2,33,94);
-    linhaCol(34,4);
-    textColor(BLUE,_DARKGRAY);
-    printf("F7 ");  textColor(BLACK,_DARKGRAY);printf("- ABRIR\t");
-    textColor(BLUE,_DARKGRAY);
-    printf("F8 ");  textColor(BLACK,_DARKGRAY);printf("- EXECUTAR\t"); 
-    textColor(BLUE,_DARKGRAY);
-    printf("F9 ");  textColor(BLACK,_DARKGRAY);printf("- MEMORIA RAM\t");
-    textColor(BLUE,_DARKGRAY);
-    printf("F10 "); textColor(BLACK,_DARKGRAY);printf("- TELA\t"); 
-    textColor(BLUE,_DARKGRAY);
-    printf("ESC "); textColor(BLACK,_DARKGRAY);printf("- SAIR\t"); 
+void telainicial()
+{
+    // lin1, col1, lin2, col2
+    box(2, 2, 33, 94);
+    linhaCol(34, 4);
+    textColor(BLUE, _DARKGRAY);
+    printf("F7 ");
+    textColor(BLACK, _DARKGRAY);
+    printf("- ABRIR\t");
+    textColor(BLUE, _DARKGRAY);
+    printf("F8 ");
+    textColor(BLACK, _DARKGRAY);
+    printf("- EXECUTAR\t");
+    textColor(BLUE, _DARKGRAY);
+    printf("F9 ");
+    textColor(BLACK, _DARKGRAY);
+    printf("- MEMORIA RAM\t");
+    textColor(BLUE, _DARKGRAY);
+    printf("F10 ");
+    textColor(BLACK, _DARKGRAY);
+    printf("- TELA\t");
+    textColor(BLUE, _DARKGRAY);
+    printf("ESC ");
+    textColor(BLACK, _DARKGRAY);
+    printf("- SAIR\t");
 }
 
-//arquivo X -> arquivo binario
+// arquivo X -> arquivo binario
 
 void imprime(char arqName[])
 {
-    char a[100],arqNameB[50];
-    int col1=3,i=3;
-    strcpy(arqNameB,arqName);
-    strcat(arqNameB,".dat");
-   
+    char a[100], arqNameB[50];
+    int col1 = 3, i = 3;
+    strcpy(arqNameB, arqName);
+    strcat(arqNameB, ".dat");
+
     telainicial();
 
-    linhaCol(2,3);
-    printf("%c%s%c",179,arqName,179);
-	FILE *arqBin = fopen(arqNameB,"rb");
-    
-    fread(&a,sizeof(char),100,arqBin);
-    while (!feof(arqBin) && i!=33)
-	{	   
-        linhaCol(i,col1);
-    	printf("%s\n",a);
-        fread(&a,sizeof(char),100,arqBin);
+    linhaCol(2, 3);
+    printf("%c%s%c", 179, arqName, 179);
+    FILE *arqBin = fopen(arqNameB, "rb");
+
+    fread(&a, sizeof(char), 100, arqBin);
+    while (!feof(arqBin) && i != 33)
+    {
+        linhaCol(i, col1);
+        printf("%s\n", a);
+        fread(&a, sizeof(char), 100, arqBin);
         i++;
-       // Sleep(500);
-       
-    }  
+        // Sleep(500);
+    }
 }
 
-void telaRam(){
-    int i=0,j=0;
-    box(2,2,33,94);
-    linhaCol(4,10),
-    printf("RAM");
-    //lin1, col1, lin2, col2
-    box(6,3,32,20);
-    for(j=6; j<32; j=j+2){   
-        for(i=4; i<20; i++){   
+void telaRam()
+{
+    int i = 0, j = 0;
+    box(2, 2, 33, 94);
+    linhaCol(4, 10),
+        printf("RAM");
+    // lin1, col1, lin2, col2
+    box(6, 3, 32, 20);
+    for (j = 6; j < 32; j = j + 2)
+    {
+        for (i = 4; i < 20; i++)
+        {
 
-            if(i==4 && j>6){
-                linhaCol(j,i-1);
-                printf("%c",195);
+            if (i == 4 && j > 6)
+            {
+                linhaCol(j, i - 1);
+                printf("%c", 195);
             }
 
-            linhaCol(j,i);
-            printf("%c",196);   
+            linhaCol(j, i);
+            printf("%c", 196);
         }
-        if(j>6){
-            linhaCol(j,i);
-            printf("%c",180);
+        if (j > 6)
+        {
+            linhaCol(j, i);
+            printf("%c", 180);
         }
-        //197 kreuz 
+        // 197 kreuz
     }
 
-    //duplicar coluna para direita, tabela de variaveis e valores(int, float e endereço)
+    // duplicar coluna para direita, tabela de variaveis e valores(int, float e endereço)
 }
 
-void gera_arq_bin(char arqName[],char *flag)
-{ 
-    char a[100],arqNameB[50];
+void gera_arq_bin(char arqName[], char *flag)
+{
+    char a[100], arqNameB[50];
 
-    strcpy(arqNameB,arqName);
-    strcat(arqNameB,".dat");
-    FILE *arq = fopen(arqName,"r");
-    
-    if(arq)
+    strcpy(arqNameB, arqName);
+    strcat(arqNameB, ".dat");
+    FILE *arq = fopen(arqName, "r");
+
+    if (arq)
     {
-    	FILE *arqBin = fopen(arqNameB,"wb");
+        FILE *arqBin = fopen(arqNameB, "wb");
 
-        fgets(a,100,arq);
-        while(!feof(arq))
+        fgets(a, 100, arq);
+        while (!feof(arq))
         {
-            fwrite(a,1,sizeof(a),arqBin);
-            fgets(a,100,arq);
+            fwrite(a, 1, sizeof(a), arqBin);
+            fgets(a, 100, arq);
         }
         fclose(arq);
         fclose(arqBin);
@@ -193,31 +217,29 @@ void gera_arq_bin(char arqName[],char *flag)
         imprime(arqName);
         *flag = '1';
     }
-    else{
-        linhaCol(8,25);
-        textColor(RED,_DARKGRAY);
+    else
+    {
+        linhaCol(8, 25);
+        textColor(RED, _DARKGRAY);
         printf("ERRO: verifique se o arquivo existe ou se eh um .C");
         fclose(arq);
-    }  
-    
+    }
 }
 
-//estruturas
-struct TpListaC{
+// estruturas
+struct TpListaC
+{
     char palav[50];
-    struct TpLista *proxC,*antC;
-
+    struct TpLista *proxC, *antC;
 };
 typedef struct TpListaC Coluna;
 
 struct Tplinha
 {
     struct TpListaC *inicio;
-    struct Tplinha *prox,*ant;
+    struct Tplinha *prox, *ant;
 };
 typedef struct Tplinha Linha;
-
-
 
 struct TpPilhaRam
 {
@@ -230,59 +252,57 @@ typedef struct TpPilhaRam Pilha;
 
 struct TpListaPont
 {
-        
-
 };
 typedef struct TpListaPont ListaP;
 
-
 // iniciar lista
-void initListaC(Coluna **L){
+void initListaC(Coluna **L)
+{
     *L = NULL;
 }
 
-void initListaL(Linha **L){
+void initListaL(Linha **L)
+{
     *L = NULL;
 }
 
-//vazia
+// vazia
 
-int isEmpetyC(Coluna *inicio){
-	return inicio == NULL;
-}
-
-int isEmpetyL(Linha *inicio){
+int isEmpetyC(Coluna *inicio)
+{
     return inicio == NULL;
 }
 
+int isEmpetyL(Linha *inicio)
+{
+    return inicio == NULL;
+}
 
-//nova caixa
+// nova caixa
 
-void criarNovaC(Coluna **col,char info[]){
-    Coluna *novo = (Coluna*) malloc (sizeof(Coluna));
+void criarNovaC(Coluna **col, char info[])
+{
+    Coluna *novo = (Coluna *)malloc(sizeof(Coluna));
     novo->proxC = novo->antC = NULL;
-    strcpy(novo->palav,info);
+    strcpy(novo->palav, info);
     *col = novo;
 }
 
-
-void criarNovaL(Linha **lin){
-     Linha *novo = (Linha*) malloc (sizeof(Linha));
-     novo->prox = novo->ant = NULL;
-     novo->inicio = NULL;
-     *lin = novo;
+void criarNovaL(Linha **lin)
+{
+    Linha *novo = (Linha *)malloc(sizeof(Linha));
+    novo->prox = novo->ant = NULL;
+    novo->inicio = NULL;
+    *lin = novo;
 }
 
-//gerar lista de lista com o codigo
-void gerarLista(Coluna **col, Linha **lin, char NomeArq[]){
-    int i=0;
+// gerar lista de lista com o codigo
+void gerarLista(Coluna **col, Linha **lin, char NomeArq[])
+{
+    int i = 0;
     char aux, auxs[50];
 
-    FILE *arq = fopen(NomeArq,"r");
-
-   
+    FILE *arq = fopen(NomeArq, "r");
 
     fclose(arq);
 }
-
-
